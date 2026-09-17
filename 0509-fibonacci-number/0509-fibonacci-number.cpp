3f67fa5f49a0ffeb13a2debe8d2faaf832ade310
ravigunawat <ravigunawat916@gmail.com>
2026-09-17 16:52:32 +0530

    Time: 13 ms (31.38%), Space: 7.6 MB (88.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
-             int fibo(int x){
-                if(x<=1) return x;
-
-                return fibo(x-1)+fibo(x-2);
-             }
-
 
     int fib(int n) {
-
-        return fibo(n);
+              if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
